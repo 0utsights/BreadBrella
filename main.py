4f9f800591bc72f86808d544900870bea1b62038
0utsights <outsightszs@outlook.com>
2025-12-02 19:56:03 -0500

    venv, and frame for pygame

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((0, 0, 0))
+    pygame.display.flip()
+
+pygame.quit()
